dafc0cc9ca27d2da48d92623f9bc032967b5d4e5
olawale-o <omoogunolawale@gmail.com>
2021-08-24 23:44:02 +0100

    Import dragons.js in configureStore.js file

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,12 @@
 import { createStore, combineReducers } from 'redux';
+import dragonsReducer from './dragons/dragons';
 import composedEnhancers from './enhancers';
 import missionsReducer from './missions/missions';
 import rocketsReducer from './rockets/rockets';
 
-const reducer = combineReducers({ rockets: rocketsReducer, mission: missionsReducer });
+const reducer = combineReducers({
+  rockets: rocketsReducer, mission: missionsReducer, dragons: dragonsReducer,
+});
 const configureStore = createStore(reducer, composedEnhancers);
 
 export default configureStore;
