d54d21ac5228230fa44592a04acb08e51ce6c343
olawale-o <omoogunolawale@gmail.com>
2021-08-24 23:39:24 +0100

    Import missions.js in configureStore.js file

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,9 @@
 import { createStore, combineReducers } from 'redux';
 import composedEnhancers from './enhancers';
+import missionsReducer from './missions/missions';
 import rocketsReducer from './rockets/rockets';
 
-const reducer = combineReducers({ rockets: rocketsReducer });
+const reducer = combineReducers({ rockets: rocketsReducer, mission: missionsReducer });
 const configureStore = createStore(reducer, composedEnhancers);
 
 export default configureStore;
