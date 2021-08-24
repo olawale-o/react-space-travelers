e42f7452281b301810c5638dd5969ebe580dab23
olawale-o <omoogunolawale@gmail.com>
2021-08-24 23:17:07 +0100

    Setup routes in App.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Route, Switch } from 'react-router';
 import './App.css';
+import Rockets from './components/Rockets';
+import Mission from './components/Mission';
+import Profile from './components/Profile';
+import Dragons from './components/Dragons';
 
-const App = () => <div>App Works</div>;
+const App = () => {
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/" exact component={Rockets} />
+        <Route path="/Mission" component={Mission} />
+        <Route path="/Profile" component={Profile} />
+        <Route path="/Dragons" component={Dragons} />
+      </Switch>
+    </div>
+  )
+}
 
 export default App;
