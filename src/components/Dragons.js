8170ae05684f17ebc85e644bd05bc2289ef83885
olawale-o <omoogunolawale@gmail.com>
2021-08-25 12:27:31 +0100

    Import Dragon component in Dragons Component

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -1,3 +1,21 @@
-const Dragons = () => <div>Dragons works</div>;
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { loadDragons } from '../redux/dragons/dragons';
+import dragonsSelector from '../redux/dragons/dragonsSelector';
+import Dragon from './Dragon';
+
+const Dragons = () => {
+  const dispatch = useDispatch();
+  const dragons = useSelector(dragonsSelector);
+  useEffect(() => {
+    dispatch(loadDragons());
+  }, []);
+  const dragonComps = dragons.map((dragon) => <Dragon key={dragon.id} dragon={dragon} />);
+  return (
+    <div className="Dragons">
+      {dragonComps}
+    </div>
+  );
+};
 
 export default Dragons;
