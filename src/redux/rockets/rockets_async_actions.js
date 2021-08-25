2223bf45c81c7515d34d1a5c69a1b907647b035a
olawale-o <omoogunolawale@gmail.com>
2021-08-25 10:54:25 +0100

    Create getRockets function in rockets_async_actions.js file

--- src/redux/rockets/rockets_async_actions.js
+++ src/redux/rockets/rockets_async_actions.js
@@ -3,9 +3,15 @@ import { loadRockets } from './rockets';
 
 const getRockets = () => (
   async function getRockets(dispatch) {
-    const allRockets  =  await fetchAllRockets();
-    console.log(allRockets);
-    dispatch(loadRockets(allRockets));
+    const allRockets = await fetchAllRockets();
+    const rockets = allRockets.map((rocket) => ({
+      id: rocket.id,
+      rocketName: rocket.rocket_name,
+      description: rocket.description,
+      flickrImages: rocket.flickr_images,
+    }));
+
+    dispatch(loadRockets(rockets));
   }
 );
 
