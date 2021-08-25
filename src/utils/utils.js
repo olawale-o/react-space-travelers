9f26ea6231ab3e594caffef6b3a38129c71c4ebe
olawale-o <omoogunolawale@gmail.com>
2021-08-25 11:05:56 +0100

    Add fetchAllMissions function utils.js file

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,11 @@
-const BASE_URI = 'https://api.spacexdata.com/v3/rockets';
-
-const fetchAllRockets = async () => {
-  const response = await fetch(BASE_URI);
+export const fetchAllRockets = async () => {
+  const ROCKETS_URI = 'https://api.spacexdata.com/v3/rockets';
+  const response = await fetch(ROCKETS_URI);
   return response.json();
 };
 
-export default fetchAllRockets;
+export const fetchAllMissions = async () => {
+  const MISSIONS_URI = 'https://api.spacexdata.com/v3/missions';
+  const response = await fetch(MISSIONS_URI);
+  return response.json();
+};
