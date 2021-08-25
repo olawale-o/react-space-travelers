c2da145c5e66bc0ba035e1cb080a70d74b521a4d
olawale-o <omoogunolawale@gmail.com>
2021-08-25 12:26:41 +0100

    Import Rocket Component in Rockets Component

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,12 +1,13 @@
 import PropType from 'prop-types';
 
 const Rocket = ({ rocket }) => {
-  const { rocket_name, description, flickr_images } = rocket;
+  console.log(rocket);
+  const { rocketName, description, flickrImages } = rocket;
   return (
     <div className="Rocket">
-      {rocket_name}
+      {rocketName}
       {description}
-      {flickr_images}
+      {flickrImages[0]}
     </div>
   );
 };
@@ -15,8 +16,8 @@ export default Rocket;
 
 Rocket.propTypes = {
   rocket: PropType.shape({
-    rocket_name: PropType.string.isRequired,
+    rocketName: PropType.string.isRequired,
     description: PropType.string.isRequired,
-    flickr_images: PropType.string.isRequired,
+    flickrImages: PropType.arrayOf(PropType.string.isRequired).isRequired,
   }).isRequired,
 };
--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,3 +1,15 @@
-const Rockets = () => <div>Rockets works</div>;
+import { useSelector } from 'react-redux';
+import rocketsSelector from '../redux/rockets/rocketsSelector';
+import Rocket from './Rocket';
+
+const Rockets = () => {
+  const rockets = useSelector(rocketsSelector);
+  const rocketComps = rockets.map((rocket) => <Rocket rocket={rocket} key={rocket.id} />);
+  return (
+    <div className="Rockets">
+      {rocketComps}
+    </div>
+  );
+};
 
 export default Rockets;
