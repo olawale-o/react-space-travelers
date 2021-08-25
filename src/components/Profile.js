2b4bf6b0e4822c80d0806df910047e56d6454887
olawale-o <omoogunolawale@gmail.com>
2021-08-25 21:01:56 +0100

    Modify Profile component in Profile.js file

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,34 +1,51 @@
+import { useSelector } from 'react-redux';
 import {
   Col, Container, Row, ListGroup,
 } from 'react-bootstrap';
+import rocketsSelector from '../redux/rockets/rocketsSelector';
+import missionsSelector from '../redux/missions/missionsSelector';
+import dragonsSelector from '../redux/dragons/dragonsSelector';
 
-const Profile = () => (
-  <div className="Profile">
-    <Container>
-      <Row>
-        <Col>
-          <h6 className="h6">My Missions</h6>
-          <ListGroup>
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col>
-          <h6 className="h6">My Rockets</h6>
-          <ListGroup>
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-          </ListGroup>
-        </Col>
-      </Row>
-    </Container>
-  </div>
-);
+const Profile = () => {
+  const rockets = useSelector(rocketsSelector);
+  const missions = useSelector(missionsSelector);
+  const dragons = useSelector(dragonsSelector);
+
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved === true)
+    .map((rocket) => (<ListGroup.Item key={rocket.id}>{rocket.rocketName}</ListGroup.Item>));
+
+  const joinedMissions = missions.filter((mission) => mission.reserved === true)
+    .map((mission) => (<ListGroup.Item key={mission.id}>{mission.missionName}</ListGroup.Item>));
+
+  const reservedDragons = dragons.filter((dragon) => dragon.reserved === true)
+    .map((dragon) => (<ListGroup.Item key={dragon.id}>{dragon.name}</ListGroup.Item>));
+
+  return (
+    <div className="Profile">
+      <Container>
+        <Row>
+          <Col>
+            <h6 className="h6">My Missions</h6>
+            <ListGroup>
+              {reservedRockets}
+            </ListGroup>
+          </Col>
+          <Col>
+            <h6 className="h6">My Dragons</h6>
+            <ListGroup>
+              {reservedDragons}
+            </ListGroup>
+          </Col>
+          <Col>
+            <h6 className="h6">My Rockets</h6>
+            <ListGroup>
+              {joinedMissions}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default Profile;
