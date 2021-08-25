97ec26fddbf2ce6a8bce7d11c46dcf20722562e6
olawale-o <omoogunolawale@gmail.com>
2021-08-25 17:01:38 +0100

    Style Profile component with react-bootstrap

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,3 +1,34 @@
-const Profile = () => <div>Profile works</div>;
+import {
+  Col, Container, Row, ListGroup,
+} from 'react-bootstrap';
+
+const Profile = () => (
+  <div className="Profile">
+    <Container>
+      <Row>
+        <Col>
+          <h6 className="h6">My Missions</h6>
+          <ListGroup>
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col>
+          <h6 className="h6">My Rockets</h6>
+          <ListGroup>
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  </div>
+);
 
 export default Profile;
