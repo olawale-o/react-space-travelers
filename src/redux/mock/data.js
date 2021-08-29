ebbcc37cf60f6807f3ada36390109b03d21ca9ea
olawale-o <omoogunolawale@gmail.com>
2021-08-29 10:49:00 +0100

    Add data.js file

---
+++ src/redux/mock/data.js
@@ -0,0 +1,27 @@
+export const rockets = [
+  {
+    id: 1,
+    rocketName: 'Falcon 1',
+    description: 'description',
+    flickrImages: ['images'],
+  },
+  {
+    id: 2,
+    rocketName: 'Falcon 9',
+    description: 'description',
+    flickrImages: ['images'],
+  }
+];
+
+export const missions = [
+  {
+    id: '1',
+    missionName: 'Thaicom',
+    description: 'description',
+  },
+  {
+    id: '2',
+    missionName: 'Telstar',
+    description: 'description',
+  }
+];
