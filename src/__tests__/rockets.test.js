74d29e2da3e0a600b011cadae2ba609d7c3a80f6
olawale-o <omoogunolawale@gmail.com>
2021-09-18 09:05:36 +0100

    Add test-utils.js file

--- src/__tests__/rockets.test.js
+++ src/__tests__/rockets.test.js
@@ -1,49 +1,62 @@
-import rocketsReducer, { reserveRocket } from '../redux/rockets/rockets';
+import React from 'react';
+import { render, screen, fireEvent } from '../test-utils';
+import Rockets from '../components/Rockets';
 
-describe('Rockets', () => {
-  test('Should return initial state', () => {
-    const initState = [];
-    expect(rocketsReducer(undefined,{})).toEqual(initState);
+describe('App Component', () => {
+  test('Should render Rockets Component', async () => {
+    render(<Rockets />);
   });
 
-  test('Should handle a rocket booking', () => {
-    const initState = [
-      {
-        id: '1',
-        rocketName: 'Falcon 9',
-        description: 'description',
-        flickrImages: 'images',
-      },
-      {
-        id: '2',
-        rocketName: 'Falcon 1',
-        description: 'description',
-        flickrImages: 'images',
-      }
-    ];
-    const rocket = {
-      id: '1',
-      rocketName: 'Falcon 9',
-      description: 'description',
-      flickrImages: 'images',
-      reserved: false,
-    };
-    const expected = [
-      {
-        id: '1',
-        rocketName: 'Falcon 9',
-        description: 'description',
-        flickrImages: 'images',
-        reserved: true,
-      },
-      {
-        id: '2',
-        rocketName: 'Falcon 1',
-        description: 'description',
-        flickrImages: 'images',
-      },
-    ];
-    expect(rocketsReducer(initState,reserveRocket(rocket.id))).toEqual(expected);
-    expect(rocketsReducer(initState,reserveRocket(rocket.id))).toHaveLength(2);
+  //  test('Should check if the links are on the page', () => {
+  //    render(<Rockets />);
+  //    expect(screen.getByText(/Rockets/i)).toBeInTheDocument();
+  //    expect(screen.getByText(/Dragons/i)).toBeInTheDocument();
+  //    expect(screen.getByText(/Missions/i)).toBeInTheDocument();
+  //    expect(screen.getByText(/MyProfile/i)).toBeInTheDocument();
+  //  });
+
+  test('Should check if a rocket Falcon 1 is present on the page', async () => {
+    render(<Rockets />);
+    expect(await screen.findByText(/Falcon 1/i)).toBeInTheDocument();
+  });
+
+  test('Should reserve Falcon 1 rocket', async () => {
+    render(<Rockets />);
+    fireEvent.click(screen.getByTestId('reserve-1'));
+    await screen.getByTestId('cancel-1');
+    expect(await screen.queryByTestId('reserved-1')).toBeInTheDocument();
+  });
+
+  test('Should Cancel Falcon 1 reservation', async () => {
+    render(<Rockets />);
+    await screen.getByTestId('cancel-1');
+    expect(await screen.queryByTestId('reserved-1')).toBeInTheDocument();
+    fireEvent.click(screen.getByTestId('cancel-1'));
+    await screen.getByTestId('reserve-1');
+    expect(await screen.queryByTestId('reserved-1')).not.toBeInTheDocument();
+  });
+
+  test('Should reserve Falcon 1 and Falcon 9 rockets', async () => {
+    render(<Rockets />);
+    fireEvent.click(screen.getByTestId('reserve-1'));
+    await screen.getByTestId('cancel-1');
+    expect(await screen.queryByTestId('reserved-1')).toBeInTheDocument();
+    fireEvent.click(screen.getByTestId('reserve-2'));
+    await screen.getByTestId('cancel-2');
+    expect(await screen.queryByTestId('reserved-2')).toBeInTheDocument();
+  });
+
+  test('Should Cancel Falcon 1 and Falcon 9 rockets reservations', async () => {
+    render(<Rockets />);
+    await screen.getByTestId('cancel-1');
+    expect(await screen.queryByTestId('reserved-1')).toBeInTheDocument();
+    await screen.getByTestId('cancel-2');
+    expect(await screen.queryByTestId('reserved-2')).toBeInTheDocument();
+    fireEvent.click(screen.getByTestId('cancel-1'));
+    await screen.getByTestId('reserve-1');
+    expect(await screen.queryByTestId('reserved-1')).not.toBeInTheDocument();
+    fireEvent.click(screen.getByTestId('cancel-2'));
+    await screen.getByTestId('reserve-2');
+    expect(await screen.queryByTestId('reserved-2')).not.toBeInTheDocument();
   });
 });
--- src/test-utils.js
+++ src/test-utils.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { render as rtlRender } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import PropType from 'prop-types';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunkMiddleWare from 'redux-thunk';
 import rocketsReducer, { LOAD_ROCKETS } from './redux/rockets/rockets';
 import missonsReducer, { LOAD_MISSIONS } from './redux/missions/missions';
-import { rockets, missions } from './redux/mock/data';
+import dragonsReducer, { LOAD_DRAGONS } from './redux/dragons/dragons';
+import { rockets, missions, dragons } from './redux/mock/data';
 
 const middlewares = [thunkMiddleWare];
 const middlewareEnhancers = applyMiddleware(...middlewares);
 
-const reducer = combineReducers({ rockets: rocketsReducer, missions: missonsReducer});
+const reducer = combineReducers({
+  rockets: rocketsReducer, missions: missonsReducer, dragons: dragonsReducer,
+});
 const store = createStore(reducer, middlewareEnhancers);
 
 const rocketsCreator = {
@@ -23,13 +28,26 @@ const missionsCreator = {
   payload: missions,
 };
 
+const dragonsCreator = {
+  type: LOAD_DRAGONS,
+  payload: dragons,
+};
+
 store.dispatch(rocketsCreator);
 store.dispatch(missionsCreator);
+store.dispatch(dragonsCreator);
 
-const Wrapper = ({ children }) => <Provider store={store}>{children}</Provider>;
-const render = (ui, options) => {
-  return rtlRender(ui, { wrapper: Wrapper, ...options });
-}
+const Wrapper = ({ children }) => (
+  <BrowserRouter>
+    <Provider store={store}>{children}</Provider>
+  </BrowserRouter>
+);
+
+Wrapper.propTypes = {
+  children: PropType.node.isRequired,
+};
+
+const render = (ui, options) => rtlRender(ui, { wrapper: Wrapper, ...options });
 
 export * from '@testing-library/react';
 export { render };
