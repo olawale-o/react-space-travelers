5512483eeae759e0dcb9a777608460850bec4824
olawale-o <omoogunolawale@gmail.com>
2021-09-18 09:04:40 +0100

    Add unit tests for rocketsReducer

---
+++ src/__tests__/redux/rockets/rockets.test.js
@@ -0,0 +1,49 @@
+import rocketsReducer, { reserveRocket } from '../../../redux/rockets/rockets';
+
+describe('Rockets', () => {
+  test('Should return initial state', () => {
+    const initState = [];
+    expect(rocketsReducer(undefined, {})).toEqual(initState);
+  });
+
+  test('Should handle a rocket booking', () => {
+    const initState = [
+      {
+        id: '1',
+        rocketName: 'Falcon 9',
+        description: 'description',
+        flickrImages: 'images',
+      },
+      {
+        id: '2',
+        rocketName: 'Falcon 1',
+        description: 'description',
+        flickrImages: 'images',
+      },
+    ];
+    const rocket = {
+      id: '1',
+      rocketName: 'Falcon 9',
+      description: 'description',
+      flickrImages: 'images',
+      reserved: false,
+    };
+    const expected = [
+      {
+        id: '1',
+        rocketName: 'Falcon 9',
+        description: 'description',
+        flickrImages: 'images',
+        reserved: true,
+      },
+      {
+        id: '2',
+        rocketName: 'Falcon 1',
+        description: 'description',
+        flickrImages: 'images',
+      },
+    ];
+    expect(rocketsReducer(initState, reserveRocket(rocket.id))).toEqual(expected);
+    expect(rocketsReducer(initState, reserveRocket(rocket.id))).toHaveLength(2);
+  });
+});
