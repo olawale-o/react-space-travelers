0b1af9c572c448be63cf019cb2e60d32b49ec039
olawale-o <omoogunolawale@gmail.com>
2021-09-18 09:02:50 +0100

    Add integrated tests for APP component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,69 +1,41 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { render, screen, fireEvent } from '../test-utils';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '../test-utils';
 import App from '../App';
 
 describe('App Component', () => {
   test('Should render App Component', async () => {
-    render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
+    render(<App />);
     // expect(screen.getByText('Missions').closest('a')).toHaveAttribute('href', '/missions');
   });
   test('Should check if the links are on the page', () => {
-    render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
+    render(<App />);
     expect(screen.getByText(/Rockets/i)).toBeInTheDocument();
     expect(screen.getByText(/Dragons/i)).toBeInTheDocument();
     expect(screen.getByText(/Missions/i)).toBeInTheDocument();
     expect(screen.getByText(/MyProfile/i)).toBeInTheDocument();
   });
 
-  test('Should check if a rocket Falcon 1 is present on the page', async () => {
-    render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
+  test('Should check if user can navigate to Missions route', async () => {
+    render(<App />);
+    userEvent.click(screen.getByText(/Missions/i));
+    expect(await screen.findByText(/Thaicom/i)).toBeInTheDocument();
+  });
+
+  test('Should check if user can navigate to Rockets route', async () => {
+    render(<App />);
+    userEvent.click(screen.getByText(/Rockets/i));
     expect(await screen.findByText(/Falcon 1/i)).toBeInTheDocument();
   });
 
-  test('Should reserve Falcon 1 rocket', async () => {
-    render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-    fireEvent.click(screen.getByTestId('reserve-1'));
-    await screen.getByTestId('cancel-1');
-    expect(await screen.queryByTestId('reserved-1')).toBeInTheDocument();
-    fireEvent.click(screen.getByTestId('cancel-1'));
-    await screen.getByTestId('reserve-1');
-    expect(await screen.queryByTestId('reserved-1')).not.toBeInTheDocument();
-  });
-
-  test('Should reserve Falcon 1 and Falcon 9 rockets', async () => {
-    render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-    fireEvent.click(screen.getByTestId('reserve-1'));
-    await screen.getByTestId('cancel-1');
-    expect(await screen.queryByTestId('reserved-1')).toBeInTheDocument();
-    fireEvent.click(screen.getByTestId('reserve-2'));
-    await screen.getByTestId('cancel-2');
-    expect(await screen.queryByTestId('reserved-2')).toBeInTheDocument();
-    fireEvent.click(screen.getByTestId('cancel-1'));
-    await screen.getByTestId('reserve-1');
-    expect(await screen.queryByTestId('reserved-1')).not.toBeInTheDocument();
-    fireEvent.click(screen.getByTestId('cancel-2'));
-    await screen.getByTestId('reserve-2');
-    expect(await screen.queryByTestId('reserved-2')).not.toBeInTheDocument();
+  test('Should check if user can navigate to MyProfile route', async () => {
+    render(<App />);
+    userEvent.click(screen.getByText(/MyProfile/i));
+    expect(await screen.findByText(/My Missions/i)).toBeInTheDocument();
+    expect(await screen.findByText(/No Missions/i)).toBeInTheDocument();
+    expect(await screen.findByText(/My Rockets/i)).toBeInTheDocument();
+    expect(await screen.findByText(/No Rockets/i)).toBeInTheDocument();
+    expect(await screen.findByText(/My Dragons/i)).toBeInTheDocument();
+    expect(await screen.findByText(/No Dragons/i)).toBeInTheDocument();
   });
 });
